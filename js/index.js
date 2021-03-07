d529c9b34a02f941aac7bb106921af51d259f1a1
Denis-Ozolin <ozolindenx@gmail.com>
2021-03-08 00:34:49 +0200

    hw is finished

--- js/index.js
+++ js/index.js
@@ -1,3 +1,9 @@
+const startBtnRef = document.querySelector('[data-action="start"]');
+const stopBtnRef = document.querySelector('[data-action="stop"]');
+const bodyRef = document.querySelector('body');
+let intervalId = null;
+let isActive = false;
+
 const colors = [
   '#FFFFFF',
   '#2196F3',
@@ -10,3 +16,21 @@ const colors = [
 const randomIntegerFromInterval = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
+
+function changeColor(){
+  bodyRef.style.backgroundColor = colors[randomIntegerFromInterval(0, colors.length -1)];
+}
+
+startBtnRef.addEventListener('click', () => {
+  if(isActive) return;
+  intervalId = setInterval(() => {
+    isActive = true;
+    changeColor();
+  }, 1000);
+});
+
+stopBtnRef.addEventListener('click', () => {
+  isActive = false;
+  clearInterval(intervalId);
+})
+
